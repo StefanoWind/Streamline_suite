ed9ab629c0be7081edb01afb519d133f0e5511fc
StefanoWind <stefano.letizia@nrel.gov>
2024-09-16 11:13:35 -0600

    finalized

--- Sampling_time.py
+++ Sampling_time.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-#03/30/2023: created, finalized
+'''
+Linear model of accumulation time
+'''
 
 import os
 cd=os.path.dirname(__file__)
@@ -17,13 +19,12 @@ matplotlib.rcParams['font.size'] = 14
 
 #%% Inputs
 source_config='config.yaml'
-source='CSM_test/*hpl'
-
+source='CSM_test/*hpl'#data folder
 percentile=50#percentile of the sampling time selected as the nominal one
-ang_tol=0.25#[deg]
+ang_tol=0.25#[deg] angular tolerance
 
 #graphics
-max_time=450#[s]
+max_time=450#[s] maximum time in the plot
 
 #%% Initialization
 with open(source_config, 'r') as fid:
@@ -33,20 +34,19 @@ with open(source_config, 'r') as fid:
 sys.path.append(config['path_utils'])
 import utils as utl
 files=glob.glob(os.path.join(cd,'data',source))
-tau_s=[]
+tau_nom=[]
 tau_min=[]
 tau_max=[]
 ppr=[]
+
 #%% Main
-
-
-
 for f in files:      
     t=[]
     ele=[]
     azi=[]
     ctr2=0
     
+    #read hpl file
     N_tot = sum(1 for line in open(f))
     Nr=int(linecache.getline(f, 3).split(':')[1])
     ppr=np.append(ppr,int(linecache.getline(f, 6).split(':')[1]))
@@ -66,11 +66,13 @@ for f in files:
         line_number=18+ctr2*(Nr+1)          
     t=t-t[0]
     
-    tau_s=np.append(tau_s,np.nanpercentile(np.diff(t),percentile))#sampling time
-    tau_min=np.append(tau_min,np.nanpercentile(np.diff(t),5))#sampling time
-    tau_max=np.append(tau_max,np.nanpercentile(np.diff(t),95))#sampling time
+    #accumulation time estimation
+    tau_nom=np.append(tau_nom,np.nanpercentile(np.diff(t),percentile))#nominal 
+    tau_min=np.append(tau_min,np.nanpercentile(np.diff(t),5))#max 
+    tau_max=np.append(tau_max,np.nanpercentile(np.diff(t),95))#min 
     linecache.clearcache()
     
+    #plot trajectory
     azi=utl.round(azi,ang_tol)%360
     ele=utl.round(ele,ang_tol)%360
     plt.figure(figsize=(18,8))
@@ -79,7 +81,7 @@ for f in files:
     plt.ylabel(r'Azimuth [$^\circ$]')
     plt.xlim([0,max_time])
     plt.grid()
-    plt.title(os.path.basename(f)+', PPR='+str(ppr[-1]))
+    plt.title(os.path.basename(f)+', PPR='+str(int(ppr[-1])))
     
     plt.subplot(2,1,2)
     plt.plot(t,ele,'.-k',markersize=1,linewidth=1)
@@ -89,13 +91,13 @@ for f in files:
     plt.grid()
     
 #linear fit
-LF=np.polyfit(ppr,tau_s,1) 
+LF=np.polyfit(ppr,tau_nom,1) 
 
 #%% Plots
 plt.figure()
-plt.errorbar(ppr,tau_s,yerr=(tau_s-tau_min,tau_max-tau_s),fmt='.k',markersize=10,capsize=5)
+plt.errorbar(ppr,tau_nom,yerr=(tau_nom-tau_min,tau_max-tau_nom),fmt='.k',markersize=10,capsize=5)
 plt.plot(ppr,LF[1]+LF[0]*ppr,'r')
-plt.title(source)
+plt.title('Source: ' +source)
 plt.text(1000,1,r'$\Delta t='+str(np.round(LF[1],3))+'+'+str(np.round(LF[0]*10**4,3))+'\cdot 10^{-4}$ PPR')
 plt.grid()
 plt.xlabel('PPR')
